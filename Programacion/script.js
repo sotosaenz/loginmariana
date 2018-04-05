3a86e20626a33bdaacadd5873ede278001b47a28
sotosaenz <35733826+sotosaenz@users.noreply.github.com>
2018-04-05 12:26:08 -0600

    Add files via upload

---
+++ Programacion/script.js
@@ -0,0 +1,9 @@
+var myweb =angular.module("myweb",[]);
+myweb.controller("login",function($scope){
+	$scope,usuario = "";
+	$scope,contra = "";
+
+	$scope.validar = function(){
+		alert("USUARIO CORRECTO")
+	}
+});
